7558e19353437932e1b3c427dd64485c7bb0f7bb
bobzhou <1957878262@qq.com>
2016-04-14 16:25:40 +0800

    alchemy demo

--- Alpha Munger/alpga_munger.py
+++ Alpha Munger/alpga_munger.py
@@ -1,5 +1,5 @@
 import os.path
-import random
+# import random
 
 import tornado.httpserver
 import tornado.ioloop
@@ -7,32 +7,34 @@ import tornado.options
 import tornado.web
 
 from tornado.options import define, options
+from request_handles.account_handler import AccountSignup
+
 define("port", default=8000, help="run on the given port", type=int)
 
 class IndexHandler(tornado.web.RequestHandler):
 	def get(self):
 		self.render('index.html')
 
-class MungedPageHandler(tornado.web.RequestHandler):
-	def post(self):
-		source_text = self.get_argument('source')
-		text_to_change = self.get_argument('change')
-		source_map = self.map_by_first_letter(source_text)
-		change_lines = text_to_change.split('\r\n')
-		self.render('munged.html', source_map=source_map, change_lines=change_lines, choice=random.choice)
+# class MungedPageHandler(tornado.web.RequestHandler):
+# 	def post(self):
+# 		source_text = self.get_argument('source')
+# 		text_to_change = self.get_argument('change')
+# 		source_map = self.map_by_first_letter(source_text)
+# 		change_lines = text_to_change.split('\r\n')
+# 		self.render('munged.html', source_map=source_map, change_lines=change_lines, choice=random.choice)
 
-	def map_by_first_letter(self, text):
-		mapped = dict()
-		for line in text.split('\r\n'):
-			for word in [x for x in line.split(' ') if len(x) > 0]:
-				if word[0] not in mapped: mapped[word[0]] = []
-				mapped[word[0]].append(word)
-		return mapped
+# 	def map_by_first_letter(self, text):
+# 		mapped = dict()
+# 		for line in text.split('\r\n'):
+# 			for word in [x for x in line.split(' ') if len(x) > 0]:
+# 				if word[0] not in mapped: mapped[word[0]] = []
+# 				mapped[word[0]].append(word)
+# 		return mapped
 
 if __name__ == '__main__':
 	tornado.options.parse_command_line()
 	app = tornado.web.Application(
-			handlers=[(r'/', IndexHandler), (r'/poem', MungedPageHandler)],
+			handlers=[(r'/', IndexHandler), (r'/poem', AccountSignup)],
 			template_path=os.path.join(os.path.dirname(__file__), "templates"),
 			static_path=os.path.join(os.path.dirname(__file__), "static"),
 			debug=True
